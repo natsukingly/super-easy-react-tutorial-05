d76a7b57ffb9ad6e7aa7cf7591a6f8d0ba32b291
natsuking <djyamaexpert@yahoo.co.jp>
2018-05-07 21:15:46 +0900

    add props

--- src/App.js
+++ src/App.js
@@ -5,13 +5,25 @@ import './App.css';
 import Title from './components/Title';
 
 class App extends Component {
+  onClickHandler = () => {
+    let title = document.getElementById('versionCounter');
+    let upgradeButton = document.getElementById('upgradeButton');
+    title.textContent = "4.0";
+    upgradeButton.style.display = "none";
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          {/* <h1 className="App-title">Hello World</h1> */}
-          <Title />
+          <Title
+            title="Hello World 3.0"
+            titleStyle={{color: 'orange'}}
+            onClick={this.onClickHandler}
+          >
+            Hello World <span id="versionCounter" style={{borderBottom: '1px solid orange'}}>3.0</span>
+          </Title>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,16 +1,20 @@
 
-// 外部のライブラリやファイルを参照するインポートの部分
 import React from 'react';
 
+const Title = (props) => {
 
-// JSXやメソッドを定義する実質上の中身
-const Title = () => {
   return(
-    <div>
-      <h2>Hello World 2.0</h2>
+    <div className="titleSection">
+      <h2
+        style={props.titleStyle}
+        id="versionStatement"
+      >{props.children}</h2>
+      <p
+        onClick={props.onClick}
+        id="upgradeButton"
+      >Upgrade</p>
     </div>
   )
 }
 
-// 外部のファイルがこのcomponentを利用できるようにするエクスポートの部分
 export default Title;
